9efc8068160b9b0b7f3cf2f042ca64ea8815d331
elissj95 <elis.jones@outlook.com>
2017-03-21 14:40:21 +0000

    Initial commit, 30% done4

---
+++ kernel/hilevel.h
@@ -0,0 +1,35 @@
+#ifndef __HILEVEL_H
+#define __HILEVEL_H
+
+// Include functionality relating to newlib (the standard C library).
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Include functionality relating to the platform.
+
+#include   "GIC.h"
+#include "PL011.h"
+#include "SP804.h"
+
+// Include functionality relating to the   kernel.
+
+#include "lolevel.h"
+#include     "int.h"
+
+
+typedef int pid_t;
+
+typedef struct {
+  uint32_t cpsr, pc, gpr[ 13 ], sp, lr;
+} ctx_t;
+
+typedef struct {
+  pid_t pid;
+  ctx_t ctx;
+} pcb_t;
+
+#endif
